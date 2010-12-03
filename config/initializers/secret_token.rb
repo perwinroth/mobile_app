19dff019b48fc72bd00a511a172835b4d5ed2dfb
Per Winroth <perwinroth@gmail.com>
2010-12-03 16:09:00 +0100

    jquery app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileApp::Application.config.secret_token = '597bf711245243f723a040b482e541ec56ee18abfcb4962bc4bbc2c02ae08ed8ca7d77b53b7545832a1d5c7451201a436baae590b5a2cdd336a311ca743a5efe'
